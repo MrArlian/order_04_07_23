e19a7c88a36c3748e2d9b78671e3f33834a48faf
MrArlia <mrarlians@gmail.com>
2023-07-14 15:12:34 +0600

    update kick command

--- handlers/user/command/kick.py
+++ handlers/user/command/kick.py
@@ -1,22 +1,37 @@
-from aiogram.utils.exceptions import ChatAdminRequired
+from aiogram.dispatcher.filters import Command
+from aiogram.utils import exceptions
 from aiogram import Bot, types
 
+from modules import tools
 from data import texts
 
 
 bot = Bot.get_current()
 
 
-async def kick_user(message: types.Message):
+async def kick_user(message: types.Message, command: Command.CommandObj):
 
-    user_id = message.from_user.id
+    reply = message.reply_to_message
     chat_type = message.chat.type
+    param = command.args or ''
     chat_id = message.chat.id
+
 
     if chat_type not in ('group', 'supergroup'):
         return await message.answer(texts.CAN_NOT_USE)
+    if reply is None and not param:
+        return await message.answer(texts.ERROR_KICK_PARAMS)
+    if reply is None and not tools.is_digit(param, only_integer=True):
+        return await message.answer(texts.ERROR_KICK_PARAM_ID)
+
+    if reply:
+        user_id = reply.from_user.id
+    else:
+        user_id = int(param)
 
     try:
         await bot.kick_chat_member(chat_id, user_id, 60)
-    except ChatAdminRequired:
-        await bot.send_message(chat_id, texts.DELETION_ERROR)
+    except exceptions.ChatAdminRequired:
+        await message.answer(texts.DELETION_ERROR)
+    except (exceptions.ChatNotFound, exceptions.InvalidUserId, exceptions.BadRequest):
+        await message.answer(texts.USER_NOT_FOUND)
